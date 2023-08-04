cb50d4466df17a0f1a85599e4b62ee69ce4b8b5c
kuanshih <kuanshih2023@gmail.com>
2023-08-04 17:31:18 +0800

    0804

--- prj_FUNShare/Controllers/CustomerController.cs
+++ prj_FUNShare/Controllers/CustomerController.cs
@@ -1,10 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prj_FUNShare.Models;
 
 namespace prj_FUNShare.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        public IActionResult PocketList(int? id)
+        {
+             FunshareContext db = new FunshareContext();
+            IEnumerable<Product> datas = db.Products.Where(p => p.PocketLists.FirstOrDefault().MemberId ==id);
+            return View(datas);
+        }
+        public IActionResult myOrder()
+        {
+            return View();
+        }
+        public IActionResult myCoupon()
+        {
+            return View();
+        }
+        public IActionResult myPoint()
+        {
+            return View();
+        }
+        public IActionResult myAccount()
         {
             return View();
         }
